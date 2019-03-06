3918bd3d674058c61b0dbb1a93d49d7e51b95056
Steven Hoffman <git@fustrate.com>
2019-03-06 15:43:52 -0800

    eslint config
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-    "rules": {
-        "no-extend-native": 0,
-        "no-param-reassign": 0,
-    },
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": "airbnb-base"
+  rules: {
+    "class-methods-use-this": 0,
+    "no-extend-native": 0,
+    "no-param-reassign": 0,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: "airbnb-base"
 };
