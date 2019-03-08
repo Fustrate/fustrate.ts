e8d958cccaf88c6a4d3433110eff7e8ba804df96
Steven Hoffman <git@fustrate.com>
2019-03-08 11:10:11 -0800

    organize methods
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -13,49 +13,21 @@ const entityMap = {
   '=': '&#x3D;',
 };
 
-function hms(seconds, zero) {
-  if (zero && (seconds === 0 || seconds === undefined)) {
-    return zero;
-  }
+const ajaxUpload = (url, data) => {
+  const formData = new FormData();
 
-  const absolute = Math.abs(seconds);
-  const parts = [
-    Math.floor(absolute / 3600),
-    `0${Math.floor((absolute % 3600) / 60).slice(-2)}`,
-    `0${Math.floor(absolute % 60).slice(-2)}`,
-  ];
+  Object.keys(data).forEach((key) => {
+    formData.append(key, data[key]);
+  });
 
-  return `${seconds < 0 ? '-' : ''}${parts.join(':')}`;
-}
-
-function postRedirectTo(href) {
-  const { csrfToken, csrfParam } = Rails;
-
-  const form = document.createElement('form');
-  let formContent = "<input name='_method' value='post' type='hidden'>";
-
-  if ((csrfParam != null) && (csrfToken != null) && !Rails.isCrossDomain(href)) {
-    formContent += `<input name='${csrfParam}' value='${csrfToken}' type='hidden'>`;
-  }
-
-  // Must trigger submit by click on a button, else "submit" event handler
-  // won't work!
-  formContent += '<input type="submit">';
-  form.method = 'post';
-  form.action = href;
-  form.innerHTML = formContent;
-  form.style.display = 'none';
-  document.body.appendChild(form);
-
-  form.querySelector('[type="submit"]').click();
-}
-
-const elementFromString = (string) => {
-  const template = document.createElement('template');
-
-  template.innerHTML = string.trim();
-
-  return template.content.firstChild;
+  return $.ajax({
+    url,
+    type: 'POST',
+    data: formData,
+    processData: false,
+    contentType: false,
+    dataType: 'script',
+  });
 };
 
 const applyMixin = (target, mixin, options) => {
@@ -98,6 +70,43 @@ const applyMixin = (target, mixin, options) => {
   }, this);
 };
 
+const elementFromString = (string) => {
+  const template = document.createElement('template');
+
+  template.innerHTML = string.trim();
+
+  return template.content.firstChild;
+};
+
+const escapeHTML = (string) => {
+  if (string === null || string === undefined) {
+    return '';
+  }
+
+  return String(string).replace(/[&<>"'`=/]/g, entity => entityMap[entity]);
+};
+
+const getCurrentPageJson = () => {
+  const pathname = window.location.pathname.replace(/\/+$/, '');
+
+  return $.get(`${pathname}.json${window.location.search}`);
+};
+
+function hms(seconds, zero) {
+  if (zero && (seconds === 0 || seconds === undefined)) {
+    return zero;
+  }
+
+  const absolute = Math.abs(seconds);
+  const parts = [
+    Math.floor(absolute / 3600),
+    `0${Math.floor((absolute % 3600) / 60).slice(-2)}`,
+    `0${Math.floor(absolute % 60).slice(-2)}`,
+  ];
+
+  return `${seconds < 0 ? '-' : ''}${parts.join(':')}`;
+}
+
 const hrefFor = (href) => {
   if (href.path) {
     return href.path();
@@ -110,40 +119,12 @@ const hrefFor = (href) => {
   throw new Error(`Invalid href: ${href}`);
 };
 
-const linkTo = (text, href, options = {}) => {
-  const element = document.createElement('a');
+const icon = (types, style = 'regular') => {
+  const classes = types.split(' ')
+    .map(thing => `fa-${thing}`)
+    .join(' ');
 
-  element.href = hrefFor(href);
-  element.innerHTML = text;
-
-  Object.keys(options).forEach((key) => {
-    element.setAttribute(key, options[key]);
-  });
-
-  return element.outerHTML;
-};
-
-const ajaxUpload = (url, data) => {
-  const formData = new FormData();
-
-  Object.keys(data).forEach((key) => {
-    formData.append(key, data[key]);
-  });
-
-  return $.ajax({
-    url,
-    type: 'POST',
-    data: formData,
-    processData: false,
-    contentType: false,
-    dataType: 'script',
-  });
-};
-
-const getCurrentPageJson = () => {
-  const pathname = window.location.pathname.replace(/\/+$/, '');
-
-  return $.get(`${pathname}.json${window.location.search}`);
+  return `<i class="fa${style[0]} ${classes}"></i>`;
 };
 
 const label = (text, type) => {
@@ -161,22 +142,6 @@ const label = (text, type) => {
   return span.outerHTML;
 };
 
-const icon = (types, style = 'regular') => {
-  const classes = types.split(' ')
-    .map(thing => `fa-${thing}`)
-    .join(' ');
-
-  return `<i class="fa${style[0]} ${classes}"></i>`;
-};
-
-const escapeHTML = (string) => {
-  if (string === null || string === undefined) {
-    return '';
-  }
-
-  return String(string).replace(/[&<>"'`=/]/g, entity => entityMap[entity]);
-};
-
 const multilineEscapeHTML = (string) => {
   if (string === null || string === undefined) {
     return '';
@@ -187,6 +152,41 @@ const multilineEscapeHTML = (string) => {
     .map(line => escapeHTML(line))
     .join('<br />');
 };
+
+const linkTo = (text, href, options = {}) => {
+  const element = document.createElement('a');
+
+  element.href = hrefFor(href);
+  element.innerHTML = text;
+
+  Object.keys(options).forEach((key) => {
+    element.setAttribute(key, options[key]);
+  });
+
+  return element.outerHTML;
+};
+
+function postRedirectTo(href) {
+  const { csrfToken, csrfParam } = Rails;
+
+  const form = document.createElement('form');
+  let formContent = "<input name='_method' value='post' type='hidden'>";
+
+  if ((csrfParam != null) && (csrfToken != null) && !Rails.isCrossDomain(href)) {
+    formContent += `<input name='${csrfParam}' value='${csrfToken}' type='hidden'>`;
+  }
+
+  // Must trigger submit by click on a button, else "submit" event handler
+  // won't work!
+  formContent += '<input type="submit">';
+  form.method = 'post';
+  form.action = href;
+  form.innerHTML = formContent;
+  form.style.display = 'none';
+  document.body.appendChild(form);
+
+  form.querySelector('[type="submit"]').click();
+}
 
 const redirectTo = (href) => {
   window.setTimeout(() => {
