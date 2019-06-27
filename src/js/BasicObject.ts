a790940c4a6f3029978546b2055b10df26caa7e7
Steven Hoffman <git@fustrate.com>
2019-06-27 09:18:45 -0700

    tweaking BasicObject.extractFromData
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- src/js/BasicObject.ts
+++ src/js/BasicObject.ts
@@ -1,51 +1,31 @@
-import moment from 'moment';
-
 import Listenable from './Listenable';
 import { deepExtend } from './object';
 
-type ObjectAttributes = { [s: string]: any };
+type JsonData = { [s: string]: any };
 
 export default class BasicObject extends Listenable {
-  public static buildList<T extends BasicObject>(items: ObjectAttributes[], attributes: ObjectAttributes = {}): T[] {
+  public static buildList<T extends BasicObject>(items: JsonData[], attributes: JsonData = {}): T[] {
     return items.map(item => <T>(new this(deepExtend(item, attributes))));
   }
 
-  public date?: string | moment.Moment;
+  public id?: number;
 
-  public createdAt?: string | moment.Moment;
-
-  public updatedAt?: string | moment.Moment;
-
-  constructor(data?: object) {
+  constructor(data?: number | string | JsonData) {
     super();
 
-    if (data) {
+    if (typeof data === 'number') {
+      this.id = data;
+    } else if (typeof data === 'string') {
+      this.id = parseInt(data, 10);
+    } else if (data) {
       this.extractFromData(data);
     }
   }
 
   // Simple extractor to assign root keys as properties in the current object.
   // Formats a few common attributes as dates with moment.js
-  public extractFromData(data?: ObjectAttributes): ObjectAttributes {
-    if (!data) {
-      return {};
-    }
-
-    Object.defineProperties(this, Object.getOwnPropertyDescriptors(data));
-
-    if (typeof this.date === 'string') {
-      this.date = moment(this.date);
-    }
-
-    if (typeof this.createdAt === 'string') {
-      this.createdAt = moment(this.createdAt);
-    }
-
-    if (typeof this.updatedAt === 'string') {
-      this.updatedAt = moment(this.updatedAt);
-    }
-
-    return data;
+  public extractFromData(data?: JsonData): JsonData {
+    return data || {};
   }
 
   public get isBasicObject(): boolean {
--- test/BasicObject.spec.ts
+++ test/BasicObject.spec.ts
@@ -1,25 +1,87 @@
 import assert from 'assert';
 import BasicObject from '../src/js/BasicObject';
 
+interface EventJsonData {
+  note: string;
+}
+
+interface RecordJsonData {
+  age: number;
+  date: string;
+  events: EventJsonData[];
+}
+
+class Event extends BasicObject {
+  public note?: string;
+  public eventable: Record;
+
+  public extractFromData(data?: EventJsonData): EventJsonData {
+    if (!data) {
+      return {};
+    }
+
+    if (data.note) { this.note = data.note; }
+    if (data.eventable) { this.eventable = data.eventable; }
+
+    return data;
+  }
+}
+
 class Record extends BasicObject {
+  public age: number;
+  public parent?: Record;
+  public date: Date;
+  public events: Event[] = [];
+
+  public extractFromData(data?: RecordJsonData): RecordJsonData {
+    if (!data) {
+      return {};
+    }
+
+    if (data.date) { this.date = new Date(data.date); }
+    if (data.events) { this.events = Event.buildList<Event>(data.events, { eventable: this }); }
+    if (data.parent) { this.parent = data.parent; }
+
+    this.age = parseInt(data.age, 10);
+
+    return data;
+  }
 }
 
 describe('::buildList', () => {
   it('builds an array of objects', () => {
-    const records = Record.buildList<Record>([{ id: 5 }, { id: 10 }, { id: 15 }]);
+    const records = Record.buildList<Record>([{ age: 5 }, { age: 10 }, { age: 15 }]);
 
     assert.strictEqual(records.length, 3);
     assert(records[0] instanceof Record);
-    assert.strictEqual(records[1].id, 10);
+    assert.strictEqual(records[1].age, 10);
   });
 
   it('builds an array of objects with extra attributes', () => {
     const parent = new Record();
-    const records = Record.buildList<Record>([{ id: 5 }], { parent });
+    const records = Record.buildList<Record>([{ age: 5 }], { parent });
 
     assert(records[0].parent === parent);
   });
 });
 
 describe('#extractFromData', () => {
+  it('sets properties from a plain object', () => {
+    const record = new Record();
+
+    record.extractFromData({ age: '5', date: '2019-06-27T00:00:00-07:00' });
+
+    assert.strictEqual(record.age, 5);
+    assert.deepStrictEqual(record.date, new Date('2019-06-27T00:00:00-07:00'));
+  });
+
+  it('creates objects from data when necessary', () => {
+    const record = new Record();
+
+    record.extractFromData({ events: [{ note: 'Hello World' }] });
+
+    assert.strictEqual(record.events.length, 1);
+    assert(record.events[0] instanceof Event);
+    assert(record.events[0].eventable === record);
+  });
 });
